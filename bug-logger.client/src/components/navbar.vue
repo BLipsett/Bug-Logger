a05059a3204bed66760cc42e62d0b974786e1f46
Brian Lipsett <lipsettbrian3@gmail.com>
2021-12-02 13:12:07 -0700

    heroku

--- bug-logger.client/src/components/navbar.vue
+++ bug-logger.client/src/components/navbar.vue
@@ -1,12 +1,8 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }" @click="getBugs">
+    <router-link class="d-flex" :to="{ name: 'Home' }" @click="getBugs">
       <div class="d-flex flex-column align-items-center">
-        <img
-          alt="logo"
-          src="../assets/img/cw-logo.png"
-          height="45"
-        />
+        <h3>Bug logger</h3>
       </div>
     </router-link>
     <button
